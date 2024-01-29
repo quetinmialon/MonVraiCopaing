fbfeafc274192116e253a74d32b2ec83c3a8607b
quentin <quentin>
2024-01-29 12:01:41 +0100

    try catch en plus

--- public/index.php
+++ public/index.php
@@ -11,13 +11,27 @@ $app1->boot();
 $app2 = App::getInstance();
 
 
-if ($app2===$app1) {
+if ($app2 === $app1) {
     var_dump($app1);
-}else{
-    echo("t'es nul bouhhh");
+} else {
+    echo ("t'es nul bouhhh");
 }
 
-$app1->singleton( fn (App $app1) => new MVC\OhPopiette(),'barbecue');
-$app1->singleton( fn(App $app1) => new MVC\OhMerguez(),'grillade');
-$app1->make('grillade');
-$app1->make('barbecue');
+$app1->singleton(fn(App $app1) => new MVC\OhPopiette(), 'barbecue');
+$app1->singleton(fn(App $app1) => new MVC\OhMerguez(), 'grillade');
+try {
+    $app1->make('patate');
+} catch (Exception $e) {
+    echo ($e);
+}
+try {
+    $app1->make('grillade');
+} catch (Exception $e) {
+    echo($e);
+};
+try {
+    $app1->make('barbecue');
+} catch (Exception $e) {
+    echo ($e);
+};
+
--- src/App.php
+++ src/App.php
@@ -3,29 +3,37 @@
 namespace MVC;
 
 
-class App {
-private static $instance;
+class App
+{
+    private static $instance;
 
-private array $container = [];
+    private array $container = [];
 
-public function singleton(\Closure $closure,String $name){
-   $value = $closure($this);
-   $this->container[$name] = $value;
-}
-public function make(string $name){
-    if(isset($this->container[$name])){
-        return dump($this->container[$name]);
-}}
-public static function getInstance() {
-    if (self::$instance === null) {
-        self::$instance = new self();
+    public function singleton(\Closure $closure, string $name)
+    {
+        $value = $closure($this);
+        $this->container[$name] = $value;
     }
+    public function make(string $name)
+    {
+        if (isset($this->container[$name])) {
+            return dump($this->container[$name]);
+        } else {
+            throw new \Exception('ça n\'existe pas ce service mon cher monsieur');
+        }
+    }
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
 
         return self::$instance;
-}
+    }
 
 
-public function boot() {
-/* on remplira dans l'atelier suivant */
-}
+    public function boot()
+    {
+        /* on remplira dans l'atelier suivant */
+    }
 }
